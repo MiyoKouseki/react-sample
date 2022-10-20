e3e799deaac128d898ae232a4934b0f981e634d7
MiyoKouseki <kouseki.miyo@gmail.com>
2022-10-20 20:50:45 +0900

    add sample1.

---
+++ sample1/src/FormApp.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+
+export class FormApp extends Component {
+
+    constructor(props) {
+	super(props);
+	this.state = {
+	    value: '',
+	    message: ''
+	};
+	this.handleInput = this.handleInput.bind(this);
+	this.send = this.send.bind(this);
+    }
+    
+    handleInput({ target: { value } }) {
+	console.log('handleInput called!');	
+	this.setState({
+	    value
+	});
+    }
+    
+    send() {
+	console.log('send called!');
+	const { value } = this.state;
+	this.setState({
+	    value: '',
+	    message: value
+	})
+    }
+    
+    render() {
+	return (
+	    <div>
+		<input type="text" value={this.state.value} onChange={this.handleInput} />
+		<button onClick={this.send}>SEND</button>
+		<div>{this.state.message}</div>
+	    </div>
+	)
+    }
+    
+}
